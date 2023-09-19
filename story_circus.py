2c6e5816babf0463b818401176ec9ad68abfce94
Jeremiah Knol <jrknol96@gmail.com>
2023-09-18 23:34:35 -0500

    Initial commit

---
+++ story_circus.py
@@ -0,0 +1,22 @@
+###############################################################################
+# Story Circus
+# by Jeremiah Knol
+#
+# Final Project for:
+#    SDEV140-12K-LB-202320-LA-81X
+#    Intro. to Software Development
+#    Ivy Tech Community College
+#
+# This is a silly random story generator.
+# The story blueprints and random word lists are loaded from files.
+# Users can select the story and word list they would like to generate.
+###############################################################################
+
+import random
+
+def main():
+    return
+
+
+if __name__ == "__main__":
+    main()

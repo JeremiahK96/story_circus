48c84ec816982f6a013fc69e6b49d6b3771d6ddf
Jeremiah Knol <jrknol96@gmail.com>
2023-09-18 23:54:49 -0500

    Add skeleton code for basic program logic

--- story_circus.py
+++ story_circus.py
@@ -14,8 +14,51 @@
 
 import random
 
+VERSION = "v0.1"
+
 def main():
-    return
+    Welcome()
+
+    mode = "NEW_GAME"
+
+    while mode != "QUIT":
+
+        if mode == "NEW_GAME":
+            story_type = getStoryType()
+            word_list = getWordList(story_type)
+
+        story = generateStory(story_type, word_list)
+        printStory(story)
+
+        mode = getNextMode()
+
+
+def Welcome():
+    print("Story Circus", VERSION)
+    print()
+    print("This is a silly story generator.")
+    print("Enjoy fun stories with randomized elements!")
+
+
+def getStoryType():
+    return ""
+
+
+def getWordList(story_type):
+    return ""
+
+
+def generateStory(story_type, word_list):
+    return "The quick red fox jumps over the lazy brown dog."
+
+
+def printStory(story):
+    print()
+    print(story)
+
+
+def getNextMode():
+    return "QUIT"
 
 
 if __name__ == "__main__":
